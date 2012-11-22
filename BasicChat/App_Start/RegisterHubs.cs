a0b84004ca575c992817eda6345d1688e601f9fa
David Fowler <davidfowl@gmail.com>
2012-11-22 03:48:32 -0800

    Initial commit

---
+++ BasicChat/App_Start/RegisterHubs.cs
@@ -0,0 +1,18 @@
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hosting.AspNet;
+
+[assembly: PreApplicationStartMethod(typeof(Samples.RegisterHubs), "Start")]
+
+namespace Samples
+{
+    public static class RegisterHubs
+    {
+        public static void Start()
+        {
+            // Register the default hubs route: ~/signalr/hubs
+            RouteTable.Routes.MapHubs();            
+        }
+    }
+}
---
+++ BasicChat/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Samples
+{
+    public class Chat : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.send(message);
+        }
+    }
+}

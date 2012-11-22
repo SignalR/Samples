aca99396901f7c94be84cfd80a5365442579a7a4
David Fowler <davidfowl@gmail.com>
2012-11-22 04:16:57 -0800

    Added console app for chat.

---
+++ BasicChatConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+
+namespace BasicChatConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        private static async Task RunAsync()
+        {
+            var connection = new HubConnection("http://localhost:44914/");
+            IHubProxy chat = connection.CreateHubProxy("Chat");
+
+            chat.On<string>("send", Console.WriteLine);
+
+            await connection.Start();
+
+            string line = null;
+            while ((line = Console.ReadLine()) != null)
+            {
+                await chat.Invoke("send", "Console: " + line);
+            }
+        }
+    }
+}
